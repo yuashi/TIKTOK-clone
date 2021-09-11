442ff887a759b566138493c589a3bfdfd5774207
yuashi <singh9ayushi9@gmail.com>
2021-09-11 18:47:44 +0530

    Connected frontend and backend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import "./App.css";
 import Video from "./Video";
+import axios from "./axios.js";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await axios.get("/api/posts");
+      setVideos(response.data);
+    };
+    fetchPost();
+  }, []);
+  console.log(videos);
+
   return (
     <div className="app">
       <div className="app_videos">
-        <Video
-          url="https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4"
-          channel="Yuashi"
-          description="This is my cute pet snail"
-          song="The Lazy Song-Bruno Mars"
-          likes={5523}
-          messages={550}
-          shares={200}
-        />
-        <Video
-          url="https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4"
-          channel="Yuashi"
-          description="My Neko !!!"
-          song="Meow Meow"
-          likes={6880}
-          messages={500}
-          shares={880}
-        />
+        {videos.map((video) => (
+          <Video
+            url={video.url}
+            channel={video.channel}
+            description={video.description}
+            song={video.song}
+            likes={video.likes}
+            messages={video.messages}
+            shares={video.shares}
+          />
+        ))}
       </div>
     </div>
   );
